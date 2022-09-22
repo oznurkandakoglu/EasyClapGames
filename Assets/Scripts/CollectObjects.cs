528d680e4ed0401d9ca38f899df62417feb8fa76
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2022-09-22 21:43:51 +0300

    v0.4

--- Assets/Scripts/CollectObjects.cs
+++ Assets/Scripts/CollectObjects.cs
@@ -12,18 +12,11 @@ public class CollectObjects : MonoBehaviour
             GetComponent<Rigidbody>().isKinematic = true;
         }
 
-        /* if (collision.collider.CompareTag("Collectable"))
-         {
-             collision.gameObject.GetComponent<FixedJoint>().connectedBody = GetComponent<Rigidbody>();
-             collision.gameObject.tag = "Untagged";
-
-         }*/
-
         if (collision.collider.CompareTag("Player"))
         {
             if(GetComponent<FixedJoint>() != null)
                 GetComponent<FixedJoint>().connectedBody = collision.gameObject.GetComponent<Rigidbody>();
-            //collision.gameObject.tag = "Untagged";
+           
 
         }
     }
