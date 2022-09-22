cebcfe6bfa6fb970d8da95b6b90c1d059d1a4683
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2022-09-22 21:39:54 +0300

    v0.3

--- Assets/Scripts/CollectObjects.cs
+++ Assets/Scripts/CollectObjects.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectObjects : MonoBehaviour
@@ -9,13 +7,23 @@ public class CollectObjects : MonoBehaviour
         
         if (collision.gameObject.CompareTag("DropArea"))
         {
-           GetComponent<FixedJoint>().connectedBody = null;                      
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            GetComponent<Rigidbody>().isKinematic = true;
         }
 
-        if (collision.collider.CompareTag("Collectable"))
+        /* if (collision.collider.CompareTag("Collectable"))
+         {
+             collision.gameObject.GetComponent<FixedJoint>().connectedBody = GetComponent<Rigidbody>();
+             collision.gameObject.tag = "Untagged";
+
+         }*/
+
+        if (collision.collider.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<FixedJoint>().connectedBody = GetComponent<Rigidbody>();
-            collision.gameObject.tag = "Untagged";
+            if(GetComponent<FixedJoint>() != null)
+                GetComponent<FixedJoint>().connectedBody = collision.gameObject.GetComponent<Rigidbody>();
+            //collision.gameObject.tag = "Untagged";
 
         }
     }
---
+++ Assets/Scripts/MultipleCollectObjects.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleCollectObjects : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("DropArea"))
+        {
+            GetComponent<FixedJoint>().connectedBody = null;
+        }
+
+        if (collision.collider.CompareTag("Collectable"))
+        {
+             collision.gameObject.GetComponent<FixedJoint>().connectedBody = GetComponent<Rigidbody>();
+             collision.gameObject.tag = "Untagged";
+        }       
+    }
+}
